b66cb53dbec9e61e1bc8762b5c6a2743d2a8bb29
MBKCHEL <mbkchelofficialdev@gmail.com>
2026-09-19 16:57:40 +0300

    feat: add RAM info

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use sysinfo::{
+    Components, Disks, Networks, System,
+};
+use std::thread;
+use std::io::{self, Write};
+use std::time::Duration;
+
 fn main() {
-    println!("Hello, world!");
+    let mut sys = System::new_all();
+
+    loop {
+        sys.refresh_all();
+
+        let total_memory = sys.total_memory() as f64 / 1024.0 / 1024.0 / 1024.0;
+        let used_memory = sys.used_memory() as f64 / 1024.0 / 1024.0 / 1024.0;
+
+        print!("\rTotal RAM: {:.2} GB / Used RAM: {:.2} GB   ", total_memory, used_memory);
+        io::stdout().flush().unwrap();
+
+        thread::sleep(Duration::from_millis(500));
+    }
 }
