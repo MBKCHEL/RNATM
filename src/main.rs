65d403b68f283ddaf4edff6a0e7ce3a793456503
MBKCHEL <mbkchelofficialdev@gmail.com>
2026-09-20 20:47:48 +0300

    feat: add CPU name

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
-use sysinfo::{
-    Components, Disks, Networks, System,
-};
-use std::thread;
 use std::io::{self, Write};
+use std::thread;
 use std::time::Duration;
+use sysinfo::{Components, Disks, Networks, System};
 
 fn main() {
     let mut sys = System::new_all();
+
+    if let Some(cpu) = sys.cpus().first() {
+        println!("\rCPU name: {}", cpu.brand());
+    }
 
     loop {
         sys.refresh_all();
@@ -14,9 +16,14 @@ fn main() {
         let total_memory = sys.total_memory() as f64 / 1024.0 / 1024.0 / 1024.0;
         let used_memory = sys.used_memory() as f64 / 1024.0 / 1024.0 / 1024.0;
 
-        print!("\rTotal RAM: {:.2} GB / Used RAM: {:.2} GB   ", total_memory, used_memory);
+        print!(
+            "\rTotal RAM: {:.2} GB / Used RAM: {:.2} GB  ",
+            total_memory, used_memory
+        );
+
         io::stdout().flush().unwrap();
 
         thread::sleep(Duration::from_millis(500));
     }
 }
+
